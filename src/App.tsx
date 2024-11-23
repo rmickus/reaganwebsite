6383d6bd60bcc8c40b64039b6b77088895c663d0
Ryan Mickus <rmickus97@gmail.com>
2024-11-22 18:06:44 -0600

    adding current time to ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 const App = (): JSX.Element => {
+  const [time, setTime] = useState<Date>(new Date());
+  useEffect(() => {
+    const int = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(int);
+    };
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello World</p>
+        <p>{time.toLocaleTimeString("en-US")}</p>
       </header>
     </div>
   );
